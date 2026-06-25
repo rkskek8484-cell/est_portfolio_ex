a5217ad9ff39d6d1806b8ebeb2d41bbd81504b7c
rkskek8484-cell <rkskek8484@gmail.com>
2026-06-25 15:55:18 +0900

    feat: 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 // import { useState } from 'react'
 import './App.css';
+import Header from './components/Header.jsx';
+import Nav from './components/Nav.jsx';
+import Figure from './components/Figure.jsx';
 
 function App() {
-  return <></>;
+  return;
+  <>
+    <h1>미션</h1>
+    <Header />
+    <Nav />
+    <Figure />
+  </>;
 }
 
 export default App;
